c57eaf4da126e6efe4fd64dd6e3f96ecb2c1646c
Alumno Plaza Vespucio <pv-alumno@duoc.acad>
2026-06-06 09:59:28 -0400

    primer commit

---
+++ src/test/java/cl/duoc/ms_citas_bbf/MsCitasBbfApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.duoc.ms_citas_bbf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsCitasBbfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
